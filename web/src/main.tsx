519cdc001fd10a90c92819b72b9cefe98163dcb5
amy <132775996+flamyamyamy@users.noreply.github.com>
2026-05-08 11:14:19 +0100

    main.txs

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,18 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import { setupDiscord } from './discord'
 
-async function bootstrap() {
-  await setupDiscord()
-
-  ReactDOM.createRoot(
-    document.getElementById('root')!
-  ).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
-
-bootstrap()
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
